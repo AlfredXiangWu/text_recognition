62efe7dfcbb2f79b12d854432a4e71d18801f8a7
Hexin <alfredxiangwu@gmail.com>
2015-07-15 21:11:17 +0800

    update

--- code/letter_split.m
+++ code/letter_split.m
@@ -1,10 +1,13 @@
 clear all;
 clc;
 
-path = '../word_train';
-error_log = fopen('../output/train_error_log.txt', 'w+');
+path = '/home/hexin/Downloads/english_words';
+error_log = fopen('/home/hexin/Downloads/english_words_output/train_error_log.txt', 'w+');
 word_list = dir(path);
 word_num = length(word_list);
+
+output_list = fopen('/home/hexin/Downloads/english_words_output/word_train_list.txt', 'w+');
+
 
 % split parameter
 letter_flag = 0;
@@ -14,11 +17,20 @@ param.pixel = 2;
 for nword = 4:word_num
     word_path = sprintf('%s/%s', path, word_list(nword).name);
     word = word_list(nword).name;
+    
+    tmp = regexp(word, ' ', 'split');
+    if length(tmp)~=1
+       word = tmp{1};
+       for nn = 1:length(tmp)-1
+          word = sprintf('%s_%s', word, tmp{nn+1}); 
+       end
+    end
+    
     param.nletter = length(word);
     img_list = dir(word_path);
     
     % save file path
-    output_path = sprintf('../output/word_train/%s', word);
+    output_path = sprintf('/home/hexin/Downloads/english_words_output/word_train/%s', word);
     mkdir(output_path);
     
     % each image 
@@ -41,13 +53,27 @@ for nword = 4:word_num
             tmp = tmp & img(j, :);
         end
         idx = find(tmp==0);
+        if isempty(idx)
+            continue;
+        end
+            
         if letter_flag
             letter = letter_seg(img, idx, param);
         else
-             save_path = sprintf('%s/%d.jpg', output_path, i-2);
-             y_start = max(1, idx(1) - param.pixel);
-             y_end = min(n, idx(end) + param.pixel);
-             save_img = img(:, y_start:y_end)*255;
+             save_path = sprintf('%s/%d.png', output_path, i-2);
+             fprintf(output_list, '%s\n', save_path);
+             
+             if length(idx) > 0.5*m
+                 save_img = uint8(255*ones(m, 2*m));                      
+                 y_start = max(1, idx(1) - param.pixel);
+                 y_end = min(n, idx(end) + param.pixel);
+                 save_img(:, 1:(y_end-y_start+1)) = img(:, y_start:y_end);
+             else
+                 y_start = max(1, idx(1) - param.pixel);
+                 y_end = min(n, idx(end) + param.pixel);
+                 save_img = img(:, y_start:y_end);
+             end
+             save_img = imresize(save_img, [50 100]);
              fprintf('%s is done!\n', save_path);
              imwrite(save_img, save_path);
              clear save_img;
